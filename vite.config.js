a712da0f51257c42f77ba2ccf96e23c7974e8dd8
jyotirmaya-bloghost <jyotirmaya7122@gmail.com>
2026-06-25 20:17:10 +0530

    package change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/community-hero/'
 })
